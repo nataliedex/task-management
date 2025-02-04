972863e65dec51d0499d600ea39b7e66583d4a5e
Natalie Blumen <natalie.blumen@gmail.com>
2025-02-04 14:08:39 -0500

    feb4

---
+++ models/Projects.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const ProjectSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    description: {
+        type: String,
+        trim: true,
+    },
+    createdBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+}, { timestamps: true }
+);
+
+module.exports = mongoose.model('Project', ProjectSchema);
---
+++ models/Task.js
@@ -0,0 +1,72 @@
+const mongoose = require('mongoose');
+
+const TaskSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true,
+      },
+      description: {
+        type: String,
+        trim: true,
+      },
+      status: {
+        type: String,
+        enum: ["To Do", "In Progress", "Completed"],
+        default: "To Do",
+      },
+      priority: {
+        type: String,
+        enum: ["Low", "Medium", "High", "Critical"],
+        default: "Medium",
+      },
+      dueDate: {
+        type: Date,
+      },
+      createdBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User", 
+        required: true,
+      },
+      assignedTo: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User", 
+      },
+      projectId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Project", 
+      },
+      comments: [
+        {
+          userId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+          },
+          text: {
+            type: String,
+            required: true,
+          },
+          createdAt: {
+            type: Date,
+            default: Date.now,
+          },
+        },
+      ],
+      isRecurring: {
+        type: Boolean,
+        default: false,
+      },
+      recurrencePattern: {
+        type: String, 
+      },
+      dependencies: [
+        {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Task", 
+        },
+      ],
+    },
+    { timestamps: true }
+);
+
+module.exports = mongoose.model('Task', TaskSchema);
